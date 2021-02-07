bfcdfcd3127f1d5f8dc1468bc86440f1b838b551
DouglasPimentel <dev.pimentel@gmail.com>
2021-02-07 18:10:01 -0300

    Hello world

---
+++ variables/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+ int score = 0;
+ int year = 2021;
+
+ std::cout << "Player score: " << score << "\n"; 
+ std::cout << "Year: " << year << "\n";
+
+ return 0;
+}
