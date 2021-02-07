fb6c150b4fe23423388e65dd77282e904897589f
DouglasPimentel <dev.pimentel@gmail.com>
2021-02-07 18:33:23 -0300

    Added temperature folder

--- variables/variable.cpp
+++ variables/variable.cpp
@@ -2,9 +2,11 @@
 
 int main() {
  int score = 0;
+ int tip = 10;
  int year = 2021;
 
  std::cout << "Player score: " << score << "\n"; 
+ std::cout << "Tip ": << tip << "\n";
  std::cout << "Year: " << year << "\n";
 
  return 0;
