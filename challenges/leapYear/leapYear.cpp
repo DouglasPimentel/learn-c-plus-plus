358cad2619795c36683c7cfd16e0cbd31d92395b
DouglasPimentel <dev.pimentel@gmail.com>
2021-02-11 23:23:29 -0300

    Logical operators

---
+++ challenges/leapYear/leapYear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int year = 0;
+
+  std::cout << "Enter year: ";
+  std::cin >> year;
+
+  if(year < 1000 || year > 9999) 
+    std::cout << "Invalid entry\n";
+  else if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
+    std::cout << year << " falls on a leap year\n";
+  else 
+    std::cout << year << " is not a leap year\n";    
+
+  return 0;
+}
