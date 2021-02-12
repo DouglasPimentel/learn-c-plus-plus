f855e27cddad28837256dbcac3036d72ba703678
DouglasPimentel <dev.pimentel@gmail.com>
2021-02-11 23:25:05 -0300

    Logical operators

---
+++ logicalOperators/and.cpp
@@ -0,0 +1,13 @@
+// Logical Operator: And
+
+#include <iostream>
+
+int main() {
+  int hunger = true;
+  int anger = true;
+
+  if(hunger == true && anger == true) 
+    std::cout << "Hangry\n";
+
+  return 0;  
+} 
---
+++ logicalOperators/or.cpp
@@ -0,0 +1,12 @@
+// Logical operator: Or
+
+#include <iostream>
+
+int main() {
+  int day = 6;
+
+  if(day == 6 || day == 7)
+    std::cout << "Weekend\n";
+
+  return 0;
+}
